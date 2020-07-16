f0f60f5eca963f17697b9426bc27d68aea930ed9
Rafael Gottardi <rafael.gottardi@c6bank.com>
2020-07-16 17:58:19 -0300

    keep walking

--- learnops-dev/foreground.sh
+++ learnops-dev/foreground.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/RafaelFino/katacoda-scenarios/master/learnops-dev/prepare.sh | bash && cd learnops && zsh
+curl https://raw.githubusercontent.com/RafaelFino/katacoda-scenarios/master/learnops-dev/prepare.sh | bash 
+cd learnops 
+zsh
